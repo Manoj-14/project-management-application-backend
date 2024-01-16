bedc07332ba00b19e22be7f704afb52a1d8f6f94
Manoj M <manumadhu1425@gmail.com>
2024-01-16 16:48:32 +0530

    Initical project setup with database

---
+++ src/test/java/com/project/projectMgmtApp/ProjectMgmtAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.projectMgmtApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMgmtAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

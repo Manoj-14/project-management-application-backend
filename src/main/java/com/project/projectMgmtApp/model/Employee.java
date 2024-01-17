3f7a7c01c68ad28ba9edd34da9b8d465726bf443
Ramakrishna G. Kannari <rkannari@BLR.ALLEGISINDIA.COM>
2024-01-17 16:30:56 +0530

    model

---
+++ src/main/java/com/project/projectMgmtApp/model/Employee.java
@@ -0,0 +1,23 @@
+package com.project.projectMgmtApp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "employee")
+public class Employee {
+
+    @Id
+    private String id;
+    private String employeeCode;
+    private String employeeName;
+
+    @DBRef
+    private UserAccount userAccountId;
+}
---
+++ src/main/java/com/project/projectMgmtApp/model/TeamMember.java
@@ -0,0 +1,24 @@
+package com.project.projectMgmtApp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "team_member")
+public class TeamMember {
+
+    @Id
+    private String id;
+    @DBRef
+    private Team teamId;
+    @DBRef
+    private Employee employeeId;
+    @DBRef
+    private Role roleId;
+}

7de173d36fdb9c47ee9e1ade0be85fc65e73bf26
Manoj M <manumadhu1425@gmail.com>
2024-01-17 14:42:36 +0530

    team controller

--- src/main/java/com/project/projectMgmtApp/team/controller/Controller.java
+++ src/main/java/com/project/projectMgmtApp/team/controller/Controller.java
@@ -1,2 +1,15 @@
-package com.project.projectMgmtApp.team.controller;public class Controller {
+package com.project.projectMgmtApp.team.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/teams")
+public class Controller {
+
+    @GetMapping
+    public String getTeam(){
+        return "All teams";
+    }
 }

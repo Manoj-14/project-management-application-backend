9a1fafa15421ee0b91edeabbcc5d8fbb95ca427c
Manoj M <manumadhu1425@gmail.com>
2024-01-18 15:33:54 +0530

    Updated primary key to string

--- src/main/java/com/project/projectMgmtApp/team/repository/AssignedRepository.java
+++ src/main/java/com/project/projectMgmtApp/team/repository/AssignedRepository.java
@@ -3,5 +3,5 @@ package com.project.projectMgmtApp.team.repository;
 import com.project.projectMgmtApp.team.model.Assigned;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface AssignedRepository extends MongoRepository<Assigned,Integer> {
+public interface AssignedRepository extends MongoRepository<Assigned,String> {
 }
